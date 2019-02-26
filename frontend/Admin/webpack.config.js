ba05eb87095b974b73b90aeffa22ae9711251f57
Andre Hadianto <andre_hadianto@mymail.sutd.edu.sg>
2019-02-27 02:39:48 +0800

    More updates on the ticket entry on the admin frontend

--- frontend/Admin/webpack.config.js
+++ frontend/Admin/webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
 module.exports = {
-//    entry: './src/app.js',
-    entry: './src/admin-ticket.js',
+    entry: './src/app.js',
+//    entry: './src/admin-ticket.js',
     output: {
         path: path.join(__dirname,'public'),
         filename: 'bundle.js'
@@ -12,7 +12,10 @@ module.exports = {
             loader: 'babel-loader',
             test: /\.js$/,
             exclude: /node_modules/
-        }]
+        },
+     {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader?limit=8192' }]
     },
     devtool: 'cheap-module-eval-source-map'
 };
