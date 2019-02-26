620a2e577f0eb19f6340ff62dc8d17f34f017ec8
Branson Lee <brannsonl@gmail.com>
2019-02-27 02:35:37 +0800

    Remove bundle and add initial login demo

--- frontend/Admin/webpack.config.js
+++ frontend/Admin/webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
 module.exports = {
-//    entry: './src/app.js',
-    entry: './src/admin-ticket.js',
+    entry: './src/app.js',
+//    entry: './src/admin-ticket.js',
     output: {
         path: path.join(__dirname,'public'),
         filename: 'bundle.js'
