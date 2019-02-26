97e8d3f4df7d33d4432de0e2968f4d53415a55a1
celine <celine110303@gmail.com>
2019-02-27 00:40:57 +0800

    include ticket routes

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,9 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 const userRoutes = require('./user/user.route.js').routes;
+const ticketRoutes = require('./ticket/ticket.route.js').routes;
 userRoutes(app);
+ticketRoutes(app);
 
 app.listen(port);
-console.log('hello celine');
+console.log('hello branson');
