4ced9f2905fa06f93e2c426a8b859d6ad5b1ecb7
celine <celine110303@gmail.com>
2019-02-27 13:49:24 +0800

    recovered env.js file

--- backend/env.js
+++ backend/env.js
@@ -4,9 +4,11 @@ const sharedHeaders = {
   'Content-Type': 'application/json',
   'Server-Token': serverToken,
 };
+const jwtSecret = process.env.JWT_SECRET;
 
 module.exports = {
     apiEndpoint,
+    jwtSecret,
     serverToken,
     sharedHeaders,
 }
