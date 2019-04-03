4ab85898cbb42c42d7230b47bff4b893090e259c
Divya Aryal <divyaaryal@ymail.com>
2019-04-03 17:08:46 +0800

    Chocolatey & meteor

--- frontend/User/src/App.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AscendersApp from './components/AscendersApp.js';
-
-ReactDOM.render(<AscendersApp/>,document.getElementById('app'));
-
